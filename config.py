01fd5cb2121b956294ccdb466b4490eb68e7541d
Jorge Cerecedo <joalceco@gmail.com>
2021-06-14 16:05:43 -0500

    postgres

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY=os.getenv("SECRET_KEY") or "faiosdfjasdf"
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    if os.getenv("DATABASE_URL"):
+        SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL").replace("postgres", "postgresql")
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
