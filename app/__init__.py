6bdc25ebdf2c032b8ef0e737988ab74504b3ee85
Jorge Cerecedo <joalceco@gmail.com>
2021-06-14 17:42:25 -0500

    admin

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 login = LoginManager(app)
 login.login_view = 'login'
+# if app.config.get('ADMIN_PASS'):
+#     from app.models import User 
+#     if not User.query.filter_by(email="admin@admin.com").first():
+#         u = User()
+#         u.create_admin(app.config.get('ADMIN_PASS'))
+#         db.session.add(u)
+#         db.session.commit()
 
 from app import routes, models
